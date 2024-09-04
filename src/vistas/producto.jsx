8bfa08360cb2e65f8985fc1197fb6f4d47b87050
Alek240825 <iglesiag1908@gmail.com>
2024-09-04 15:32:14 -0500

    Arreglos

--- src/vistas/producto.jsx
+++ src/vistas/producto.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { StarIcon, HeartIcon, ShareIcon, ShoppingCartIcon } from 'lucide-react';
