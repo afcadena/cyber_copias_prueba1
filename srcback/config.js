34186e42e0a1916c9cdecc8dc9e95a1a7b068b02
afcadena <afcadena022@gmail.com>
2024-10-20 18:18:36 -0500

    se actualizo la conexion de la base de datos a mongo atlas

--- srcback/config.js
+++ srcback/config.js
@@ -1,5 +1,5 @@
 // srcback/config.js
 export const PORT = 4000; // O cambia a otro puerto si es necesario
-export const MONGODB_URI = "mongodb://localhost:/cybercopiasfinal";
+export const MONGODB_URI = "mongodb+srv://afcadena022:1013112289Fe@cybercopias.hgvmh.mongodb.net/cybercopiasfinal?retryWrites=true&w=majority";
 export const TOKEN_SECRET = "15141312"; // Usa una clave secreta fuerte
 export const FRONTEND_URL = "http://localhost:5173";
